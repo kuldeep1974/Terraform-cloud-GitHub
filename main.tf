5087b66a4a3ad4463f7b8951eeeeabb6a0b939ce
Kuldeep Singh <singhkuldeep4168@gmail.com>
2025-09-15 23:08:09 +0530

    chatgpt

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 terraform {
-    required_version = "~> 1.13.2" // added latest version
-    required_providers {
+  required_version = "~> 1.13.2"
+
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
-     }
+      version = "~> 5.0"
     }
-   }
+  }
+}
 
 provider "aws" {
-        region = "us-east-1" 
-        #profile = "default"
-	}
+  region = "us-east-1"
+}
 
 resource "aws_instance" "kuldeepinfra" {
-	ami = "ami-0c2b8ca1dad447f8a" 
-	instance_type = "t2.nano"
+  ami           = "ami-0c2b8ca1dad447f8a"
+  instance_type = "t2.nano"
 }
